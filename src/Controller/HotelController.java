4967dcd65fa4e2c7f03b1808606376fe998f4d88
praveen babu <rpraveenbabu851@gmail.com>
2022-05-24 11:44:33 +0530

    modi

--- src/Controller/HotelController.java
+++ src/Controller/HotelController.java
@@ -1,5 +1,6 @@
 package Controller;
 import Models.Hotels;
+import Models.Food;
 import java.util.Scanner;
 import java.util.ArrayList;
 public class HotelController{
@@ -16,22 +17,40 @@ public class HotelController{
                 String name = scanner.nextLine();
                 Hotels hotel = new Hotels(name);
                 hotelList.add(hotel);
+                
             case 2:
                 System.out.println("Select Your hotel from the list");
-                HotelController.display();
+                HotelController.displayHotels();
                 System.out.println("Enter Your Choice....");
                 System.out.println("1.Add food");
                 System.out.println("2.Remove food");
-                int choice = Integer.parseInt(scanner.nextLine());
+                int hotelChoice = Integer.parseInt(scanner.nextLine());
                 switch(choice){
-                    case 1:
+                    case 1: 
+                    System.out.println("Enter the food item to add");
+                    String food = scanner.nextLine();
+                    System.out.println("Enter the quantity");
+                    long quantity = Long.parseLong(scanner.nextLine());
+                    System.out.println("Enter the price");
+                    double price = Double.parseDouble(scanner.nextLine()); 
+                    hotelList.get(hotelChoice).addFood(new Food(food, quantity, price));
+                    case 2:
+                    System.out.println("Select the food item to remove");
+                    HotelController.displayFood();
+                    int FoodChoice = Integer.parseInt(scanner.nextLine());
+                    hotelList.get(hotelChoice).removeFood(Hotels.FoodItems.get(FoodChoice));                    
 
                 }
         }
     }
-    public static void display(){
-        for(Hotels hotel:hotelList){
-            System.out.println(1+"."+hotel.getName());
+    public static void displayHotels(){
+        for(int i = 0; i < hotelList.size(); i++){
+            System.out.println((i+1)+"."+hotelList.get(i).getName());
+        }
+    }
+    public static void displayFood(){
+        for(int i = 0; i < Hotels.FoodItems.size(); i++){
+            System.out.println((i+1)+"."+Hotels.FoodItems.get(i).getName());
         }
     }
 }
